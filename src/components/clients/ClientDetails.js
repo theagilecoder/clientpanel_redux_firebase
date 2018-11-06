7aef344838de7ed9eae98d173361fd1177bd5fde
Pushkar Singh <theagilecoder@gmail.com>
2018-11-06 16:12:13 +0530

    Login Functionality

--- src/components/clients/ClientDetails.js
+++ src/components/clients/ClientDetails.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { firestoreConnect } from "react-redux-firebase";
-import PropTypes from "prop-types";
 import Spinner from "../layout/Spinner";
 import classnames from "classnames";
 
